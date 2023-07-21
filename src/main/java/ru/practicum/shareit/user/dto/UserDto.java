62004512f5b8a716a3e53732b05641813a170f65
Galina <Galya_iv@mail.ru>
2023-07-21 23:28:32 +0300

    Готовое ТЗ 14 обновлённые user

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,26 +1,48 @@
 package ru.practicum.shareit.user.dto;
+//
+//import lombok.AccessLevel;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.experimental.FieldDefaults;
+//
+//import javax.validation.constraints.Email;
+//import javax.validation.constraints.NotBlank;
+//import javax.validation.constraints.NotNull;
+//
+//@Data
+//@Builder
+//@FieldDefaults(level = AccessLevel.PRIVATE)
+//public class UserDto {
+//    Long id;
+//
+//    @NotNull(message = "Имя пользователя не заданно")
+//    @NotBlank(message = "Имя пользователя не может состоять из пустой строки")
+//    private String name;
+//
+//    @NotNull(message = "email пользователя не задан")
+//    @NotBlank(message = "email пользователя не может состоять из пустой строки")
+//    @Email(message = "Не верный формат email")
+//    private String email;
+//}
 
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
-import lombok.experimental.FieldDefaults;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDto {
-    Long id;
+    private Long id;
 
-    @NotNull(message = "Имя пользователя не заданно")
-    @NotBlank(message = "Имя пользователя не может состоять из пустой строки")
+    @NotNull
+    @NotEmpty
     private String name;
 
-    @NotNull(message = "email пользователя не задан")
-    @NotBlank(message = "email пользователя не может состоять из пустой строки")
-    @Email(message = "Не верный формат email")
+    @Email
+    @NotNull
+    @NotEmpty
     private String email;
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,22 +1,46 @@
 package ru.practicum.shareit.user.model;
 
+//import lombok.*;
+//import lombok.experimental.FieldDefaults;
+//
+//import javax.persistence.*;
+//
+///**
+// * TODO Sprint add-controllers.
+// */
+//@Entity
+//@Table(name = "users")
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@FieldDefaults(level = AccessLevel.PRIVATE)
+//
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(name = "name", nullable = false)
+//    private String name;
+//
+//    @Column(name = "email", nullable = false, unique = true)
+//    private String email;
+//}
+
 import lombok.*;
-import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
 
-/**
- * TODO Sprint add-controllers.
- */
-@Entity
-@Table(name = "users")
 @Data
-@Builder
+@Entity
+@Table(name = "users", schema = "public")
+@EqualsAndHashCode(exclude = {"name", "email"})
 @NoArgsConstructor
 @AllArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
-
+@Builder(toBuilder = true)
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
