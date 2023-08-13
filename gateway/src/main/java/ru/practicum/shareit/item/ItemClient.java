b944c3fe79d6b0ee311a6853ddf6a23fb90087fb
Galina <Galya_iv@mail.ru>
2023-08-13 11:26:31 +0300

    Исправленное ТЗ 16

--- gateway/src/main/java/ru/practicum/shareit/item/ItemClient.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemClient.java
@@ -1,11 +1,10 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.client.BaseClient;
 import ru.practicum.shareit.item.dto.CommentDto;
@@ -13,12 +12,11 @@ import ru.practicum.shareit.item.dto.ItemDto;
 
 import java.util.Map;
 
-@Service
+@Component
 public class ItemClient extends BaseClient {
 
     private static final String API_PREFIX = "/items";
 
-    @Autowired
     public ItemClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
--- gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
@@ -1,23 +1,22 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.client.BaseClient;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 
 import java.util.Map;
 
-@Service
+@Component
 public class ItemRequestClient extends BaseClient {
 
     private static final String API_PREFIX = "/requests";
 
-    @Autowired
+
     public ItemRequestClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
